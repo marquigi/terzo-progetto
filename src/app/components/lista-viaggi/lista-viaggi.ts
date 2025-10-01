4de08c63a88fa2851197d5f4a622780c6aab0876
Mark Luigi <markluigi@yahoo.it>
2025-10-01 18:58:06 +0200

    .

--- src/app/components/lista-viaggi/lista-viaggi.ts
+++ src/app/components/lista-viaggi/lista-viaggi.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Post } from '../../models/blog';
+import { DatiService } from '../../services/dati-service';
+import { ViaggiItem } from "../viaggi-item/viaggi-item";
 
 @Component({
   selector: 'app-lista-viaggi',
-  imports: [],
+  imports: [ViaggiItem],
   templateUrl: './lista-viaggi.html',
   styleUrl: './lista-viaggi.css'
 })
-export class ListaViaggi {
+export class ListaViaggi implements OnInit {
+
+
+  postViaggi: Post[] = [];
+
+  datiServer: DatiService = inject(DatiService);
+
+  ngOnInit(): void {
+    this.postViaggi = this.datiServer.getDati();
+  }
 
 }
--- src/app/components/viaggi-item/viaggi-item.ts
+++ src/app/components/viaggi-item/viaggi-item.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Post } from '../../models/blog';
 
 @Component({
   selector: 'app-viaggi-item',
@@ -7,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './viaggi-item.css'
 })
 export class ViaggiItem {
-
+  @Input()
+  viaggi?: Post;
 }
