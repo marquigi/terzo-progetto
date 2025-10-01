e5394101de53b03c06cf4a24107b3363f4388660
Mark Luigi <markluigi@yahoo.it>
2025-10-01 22:14:50 +0200

    .

--- src/app/components/lista-preferiti/lista-preferiti.ts
+++ src/app/components/lista-preferiti/lista-preferiti.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { PreferitiService } from '../../services/preferiti-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lista-preferiti',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './lista-preferiti.html',
   styleUrl: './lista-preferiti.css'
 })
